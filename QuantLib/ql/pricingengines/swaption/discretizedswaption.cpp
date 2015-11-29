7b950b5338bcbda11fce2ae06fbfb59f1b69c1d7
Eric Ehlers <eric.ehlers@reposit.be>
2015-11-29 17:26:23 +0100

    comment out undefined variable

--- QuantLib/ql/pricingengines/swaption/discretizedswaption.cpp
+++ QuantLib/ql/pricingengines/swaption/discretizedswaption.cpp
@@ -91,7 +91,7 @@ namespace QuantLib {
                                                                 referenceDate,
                                                                 dayCounter, positiveDates));
         underlyingAsSwapStrip_ = boost::dynamic_pointer_cast<DiscretizedCoterminalSwapStrip>(underlying_);
-        QL_REQUIRE(underlyingAsSwapStrip_ != nullptr, "Underlying must be a DiscretizedCoterminalSwapStrip object.");
+        QL_REQUIRE(underlyingAsSwapStrip_ /*!= nullptr*/, "Underlying must be a DiscretizedCoterminalSwapStrip object.");
     }
 
     const boost::shared_ptr<std::vector<std::pair<bool, std::pair<Size, Real> > > > DiscretizedSwaption::exerciseIndex() const {
